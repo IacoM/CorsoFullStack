f4cf4bd3c7570796e337c99cd5b4ed5310a7a3f9
Giuseppe Iacovelli <giuseppeiacovelli01@gmail.com>
2023-01-11 14:05:19 +0100

    ArrayAndLocalStorage

---
+++ Javascript/ArrayMethodsAdvanced/3ArrayMethodsAdvanced.js
@@ -0,0 +1,21 @@
+function calculateAverageAge(persons) {
+  const sum = persons.reduce((index, value) => index + value.age, 0 );
+  media = sum / 10;
+  return Math.round(media)
+}
+const persons = [
+  { name: 'Paul', age: 16 },
+  { name: 'George', age: 17 },
+  { name: 'Lucas', age: 21 },
+  { name: 'Marco', age: 32 },
+  { name: 'Peter', age: 18 },
+  { name: 'Carl', age: 13 },
+  { name: 'Simon', age: 24 },
+  { name: 'Mark', age: 15 },
+  { name: 'Sandra', age: 34 },
+  { name: 'Alice', age: 28 }
+];
+
+const average = calculateAverageAge(persons);
+console.log(persons);
+console.log('Average age is: ', average);
