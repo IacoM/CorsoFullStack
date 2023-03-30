f750dd09518e659641401bff3643dadb3c45414d
Giuseppe Iacovelli <giuseppeiacovelli01@gmail.com>
2023-03-30 18:59:21 +0200

    EndTypescript

---
+++ Typescript/exercise-22-construct-types-with-record-start.ts
@@ -0,0 +1,45 @@
+/**
+ * START: Follow the instructions below.
+ */
+
+// Fix this use of the `Record` utility type so it constructs a valid type.
+
+const currency: Record<CurrencyNations, Currency> = {
+    name: "United States dollar",
+    code: "USD",
+    symbol: "$",
+};
+
+// Add a type annotation to the `currencies` variable that uses
+// the `Record` utility type.
+// Hint: Pass the `Currency` type as the second type argument to `Record<>`.
+type CurrencyNations = "france" | "china" | "colombia";
+
+
+interface Currency {
+    name: string;
+    code: string;
+    symbol: string;
+}
+
+const currencies: Record<CurrencyNations, Currency> = {
+    france: {
+        name: "Euro",
+        code: "EUR",
+        symbol: "€",
+    },
+    china: {
+        name: "Renminbi",
+        code: "CNY",
+        symbol: "¥",
+    },
+    colombia: {
+        name: "Colombian peso",
+        code: "COP",
+        symbol: "$",
+    }
+};
+
+// ----
+
+export {};
